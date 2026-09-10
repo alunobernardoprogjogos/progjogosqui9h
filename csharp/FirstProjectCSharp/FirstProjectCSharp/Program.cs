b01be5903233626f7ba527933da9d21ab0267f07
alunobernardoprogjogos <alunobernardoprogjogosqui9h@cursosposrio.com.br>
2026-09-10 10:53:58 -0300

    Organizing my project

---
+++ csharp/FirstProjectCSharp/FirstProjectCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstProjectCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite seu nome: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Olá, " + name);
+
+        }
+    }
+}
